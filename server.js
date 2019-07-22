78b782f3bcd2a11423489cfcf54c21385e49bb44
Marine809 <rthorpesr@verizon.net>
2019-07-22 15:11:08 -0400

    wishes

--- server.js
+++ server.js
@@ -24,49 +24,51 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
-
-// Root get route
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM wishes;", function(err, data) {
-    if (err) throw err;
-
-    // Test it
-    console.log('The solution is: ', data);
-
-    // Test it
-    // return res.send(data);
-
-    res.render("index", { tasks: data });
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
   });
-});
-
-// Post route -> back to home
-app.post("/", function(req, res) {
-  // Test it
-  console.log('You sent, ' + req.body.task);
-
-  // Test it
-  return res.send('You sent, ' + req.body.task);
-
-  // When using the MySQL package, we'd use ?s in place of any values to be inserted, which are then swapped out with corresponding elements in the array
-  // This helps us avoid an exploit known as SQL injection which we'd be open to if we used string concatenation
-  // https://en.wikipedia.org/wiki/SQL_injection
-  connection.query("INSERT INTO wishes (wish) VALUES (?)", [req.body.task], function(err, result) {
-    if (err) throw err;
-
-    res.redirect("/");
+  
+  // Root get route.
+  app.get("/", function(req, res) {
+    connection.query("SELECT * FROM wishes;", function(err, data) {
+      if (err) {
+        throw err;
+      }
+  
+      // Test it.
+      // console.log('The solution is: ', data);
+  
+      // Test it.
+      // res.send(data);
+  
+      res.render("index", { wishes: data });
+    });
   });
-});
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+  
+  // Post route -> back to home
+  app.post("/", function(req, res) {
+    // Test it.
+    // console.log('You sent, ' + req.body.wish);
+  
+    // Test it.
+    // res.send('You sent, ' + req.body.wish)
+  
+    connection.query("INSERT INTO wishes (wish) VALUES (?)", [req.body.wish], function(err, result) {
+      if (err) {
+        throw err;
+      }
+  
+      res.redirect("/");
+    });
+  });
+  
+  // Start our server so that it can begin listening to client requests.
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
